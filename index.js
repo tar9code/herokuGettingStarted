1d80a19db6c7e46c14071893d5fb6a462bc073cf
TAdmin <TAdmin>
2018-04-17 13:41:57 -0700

    5

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ app.get('/cool', function(request, response) {
 });
 
 var pg = require('pg');
+var pool = new pg.Pool()
 app.get('/db', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    pool.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM test_table', function(err, result) {
             done();
             if (err)
