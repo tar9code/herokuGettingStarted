8c34295e3cc03e7b2e7ab4fd17460ddd275ba4eb
tar9code <tslbavg@gmail.com>
2018-04-24 12:52:33 -0700

    fixed user

--- index.js
+++ index.js
@@ -48,13 +48,14 @@ module.exports = router;
 // ***** mongodb get all of the Routes in Routes collection where frequence>=1
 //      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
 //
-var mongoRouteUrl = 'mongodb://heroku_pmk6n54s:penh0a964unc8citdi3c1943cv@ds153869.mlab.com:53869/heroku_pmk6n54s';
+//var mongoRouteUrl = 'mongodb://heroku_pmk6n54s:penh0a964unc8citdi3c1943cv@ds153869.mlab.com:53869/heroku_pmk6n54s';
+var mongoRouteUrl = 'mongodb://heroku_nhr73h2j:m925ongoDB@ds149279.mlab.com:49279/heroku_nhr73h2j';
 router.get('/mongodb', function (request, response)
 {
     mongodb.MongoClient.connect(mongoRouteUrl, function(err, client) {
         mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, db) {  // works with mongodb v2 but not v3
             if (err) throw err;         //get collection of routes
-            var db = client.db('heroku_pmk6n54s');  // in v3 we need to get the db from the client
+            var db = client.db('heroku_nhr73h2j');  // in v3 we need to get the db from the client
             var Routes = db.collection('Routes');         //get all Routes with frequency >=1
             Routes.find({frequency: {$gte: 0}}).sort({name: 1}).toArray(function (err, docs) {
                 if (err) throw err;
